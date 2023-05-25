bcd67ee14b084db13de41c6f2548b4043813ec43
chicong <chicong115@gmail.com>
2023-05-25 15:40:42 +0700

    added value

--- dist/index.js
+++ dist/index.js
@@ -17023,11 +17023,11 @@ const process = __nccwpck_require__(7282);
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    var name = core.getInput('who-to-greet') ? core.getInput('who-to-greet') : process.env['who-to-greet'];
+    var name = core.getInput('who-to-greet') ? core.getInput('who-to-greet') : 'cong';
 
     var repo = core.getInput('repo') ? core.getInput('repo')
-    : process.env['repo'];
-    var app_name = core.getInput('app-name') ? core.getInput('app-name') : process.env['app-name'];
+    : 'https://github.com/gabyshev/testapp';
+    var app_name = core.getInput('app-name') ? core.getInput('app-name') : 'testapp';
 
     console.log(`Hello ${name}`);
 
@@ -17037,35 +17037,35 @@ async function run() {
 
 
     
-    // console.log(JSON.stringify(repo, null, '\t'));
+    console.log(JSON.stringify(repo, null, '\t'));
 
 
 
-    // if(!fs.existsSync(`./${app_name.toString()}`)){
-    //   // shell.cd('./')
-    //   // shell.exec(`git clone ${repo.toString()}` )
-    //   // var files = fs.readdirSync(`./${app_name.toString()}`);
+    if(!fs.existsSync(`./${app_name.toString()}`)){
+      shell.cd('./')
+      shell.exec(`git clone ${repo.toString()}` )
+      var files = fs.readdirSync(`./${app_name.toString()}`);
   
       
-    //   // console.log(JSON.stringify(files, null, '\t'));
+      console.log(JSON.stringify(files, null, '\t'));
   
   
-    //   // core.setOutput("files : ", files.toString());
+      core.setOutput("files : ", files.toString());
   
   
-    //   // console.log(JSON.stringify(github, null, '\t'));
-    // }else{
-    //   // var file_s = fs.readdirSync('./');
-    //   // console.log(JSON.stringify(file_s, null, '\t'));
-    //   // shell.cd(`./${app_name.toString()}`)
-    //   // shell.exec(`git fetch && git checkout main` )
-    //   // file_s = fs.readdirSync('./');
-    //   // console.log(JSON.stringify(file_s, null, '\t'));
+      console.log(JSON.stringify(github, null, '\t'));
+    }else{
+      var file_s = fs.readdirSync('./');
+      console.log(JSON.stringify(file_s, null, '\t'));
+      shell.cd(`./${app_name.toString()}`)
+      shell.exec(`git fetch && git checkout main` )
+      file_s = fs.readdirSync('./');
+      console.log(JSON.stringify(file_s, null, '\t'));
   
   
-    //   // core.setOutput("files : ", file_s.toString());
+      core.setOutput("files : ", file_s.toString());
 
-    // }
+    }
 
     
   } catch (error) {
